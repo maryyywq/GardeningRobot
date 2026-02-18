a358711d132cac1b72d5ab7b7bc0537e0ffdb20f
Maria <mk902589@yandex.ru>
2026-02-18 21:18:26 +0700

    Add power source implementations

---
+++ src/FuelCell.java
@@ -0,0 +1,6 @@
+public class FuelCell implements IPowerSource {
+    private double level = 100.0;
+    @Override public void charge() { level = 100.0; System.out.println("Источник питания на основе топлива: заправка завершена"); }
+    @Override public double getLevel() { return level; }
+    @Override public void switchToBackup() { System.out.println("Источник питания на основе топлива: переход на резервный бак"); }
+}
