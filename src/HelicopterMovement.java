73f4c350429a5960df5529098428731b66590218
Maria <mk902589@yandex.ru>
2026-02-18 21:09:20 +0700

    Add movement system implementations

---
+++ src/HelicopterMovement.java
@@ -0,0 +1,8 @@
+public class HelicopterMovement implements IMovementSystem {
+    private double speed = 2.0;
+    @Override public void moveTo(Location target) {
+        System.out.println("Система передвижения с винтом сверху (как у вертолета): полёт к " + target);
+    }
+    @Override public void stop() { System.out.println("Система передвижения с винтом сверху (как у вертолета): зависание"); }
+    @Override public void setSpeed(double speed) { this.speed = speed; System.out.println("Система передвижения с винтом сверху (как у вертолета): скорость установлена " + speed); }
+}
