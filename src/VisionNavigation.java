5f482b47f3c88a98c5b552907b0dcd23850264ab
Maria <mk902589@yandex.ru>
2026-02-18 21:14:02 +0700

    Added classes for types of navigation

---
+++ src/VisionNavigation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class VisionNavigation implements INavigation {
+    private Location current = new Location(0, 0);
+    @Override public void updatePosition() {
+        System.out.println("Навигация с помощью компьютерного зрения: обновление позиции по камерам");
+    }
+    @Override public Route planRoute(Location start, Location goal) {
+        System.out.println("Навигация с помощью компьютерного зрения: планирование маршрута от " + start + " до " + goal + " на основе изображений");
+        return new Route(Arrays.asList(start, goal));
+    }
+    @Override public void adjustRoute(Obstacle obstacle) {
+        System.out.println("Навигация с помощью компьютерного зрения: обнаружено препятствие в " + obstacle.position + ", пересчёт маршрута");
+    }
+
+}
