a11579c5c45070d1af9afeaca4100aed8e87c4ba
Maria <mk902589@yandex.ru>
2026-02-18 20:46:43 +0700

    Added additional classes

---
+++ src/Obstacle.java
@@ -0,0 +1,4 @@
+public class Obstacle {
+    public final Location position;
+    public Obstacle(Location pos) { this.position = pos; }
+}
---
+++ src/Task.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Task {
+    public final String type;
+    public final Map<String, Object> params;
+    public Task(String type, Map<String, Object> params) { this.type = type; this.params = params; }
+}
