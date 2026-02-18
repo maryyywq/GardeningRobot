266e06e32c92fcbf9e42875fb860b10d7028b3dc
Maria <mk902589@yandex.ru>
2026-02-18 21:52:15 +0700

    Added abstract class Robot

---
+++ src/Robot.java
@@ -0,0 +1,45 @@
+abstract class Robot implements IRobot {
+    protected String id;
+    protected RobotStatus status = RobotStatus.IDLE;
+    protected IMovementSystem movementSystem;
+    protected INavigation navigation;
+    protected IPowerSource powerSource;
+    protected ICommunication communication;
+    protected IKnowledgeBase knowledgeBase;
+    protected ITool currentTool;
+    protected Location location;
+
+    public Robot(String id, IMovementSystem ms, INavigation nav, IPowerSource ps,
+                 ICommunication comm, IKnowledgeBase kb, Location startLoc) {
+        this.id = id;
+        this.movementSystem = ms;
+        this.navigation = nav;
+        this.powerSource = ps;
+        this.communication = comm;
+        this.knowledgeBase = kb;
+        this.location = startLoc;
+    }
+
+    @Override public void startTask() { status = RobotStatus.WORKING; System.out.println(id + ": задача запущена"); }
+    @Override public void stopTask() { status = RobotStatus.IDLE; System.out.println(id + ": задача остановлена"); }
+    @Override public RobotStatus getStatus() { return status; }
+    @Override public void receiveCommand(String command) {
+        System.out.println(id + ": получена команда: " + command);
+        communication.sendData("Подтверждение", "контроллер");
+    }
+
+    @Override
+    public void setTool(ITool tool) {
+        if (canUseTool(tool)) {
+            this.currentTool = tool;
+            System.out.println(id + ": установлен инструмент " + tool.getClass().getSimpleName());
+        } else {
+            System.out.println(id + ": невозможно использовать инструмент " + tool.getClass().getSimpleName() + " - несовместим с моей базой знаний");
+        }
+    }
+
+    @Override
+    public boolean canUseTool(ITool tool) {
+        return knowledgeBase.isToolCompatible(tool);
+    }
+}
