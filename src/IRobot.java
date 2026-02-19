f7a349d847c4730d5e1e30e20948ce53f3fc26b9
Maria <mk902589@yandex.ru>
2026-02-19 16:27:53 +0700

    Added class RobotProxy

--- src/IRobot.java
+++ src/IRobot.java
@@ -1,8 +1,14 @@
-public interface IRobot {
+interface IRobot {
     void startTask();
     void stopTask();
     RobotStatus getStatus();
     void setTool(ITool tool);
     void receiveCommand(String command);
     boolean canUseTool(ITool tool);
+    IMovementSystem getMovementSystem();
+    INavigation getNavigation();
+    IPowerSource getPowerSource();
+    ICommunication getCommunication();
+    IKnowledgeBase<?> getKnowledgeBase();
+    ITool getCurrentTool();
 }
